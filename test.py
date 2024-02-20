6114ab0cbe579ac3f44237ab29272b0c9c56df84
JebK00 <86977267+JebK00@users.noreply.github.com>
2024-02-20 14:45:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import pyxel
 
-pyxel.init(160, 120)
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)
+        self.x = 0
+        pyxel.run(self.update, self.draw)
+    
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
 
-def update():
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(20,20,20,20,11)
+        pyxel.rect(60,60,20,50,10)
 
-def draw():
-    pyxel.cls(0)
-    pyxel.rect(10,10,20,20,11)
-
-pyxel.run(update, draw)
+App()
